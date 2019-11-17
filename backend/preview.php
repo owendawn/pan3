1e63512629404731d418534b44e7a8a979bd6f6a
owen <10123881452QQ.COM>
2019-11-17 19:45:43 +0800

    fine turn

---
+++ backend/preview.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: owen pan
+ * Date: 2016/12/13
+ * Time: 10:05
+ */
+$url = $_REQUEST["url"];
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+</head>
+<body style="text-align:center">
+    <img style="margin:auto" src="<?php echo $url?>">
+</body>
+</html>
